c36645b747d738bb2580891fc8f04752609667a9
andikastr <andikasatria8248@gmail.com>
2024-05-26 13:31:12 +0700

    little changes

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+
+const ctx = document.getElementById('yearChart').getContext('2d');
+        const myChart = new Chart(ctx, {
+            type: 'line',  // Type of chart (can be 'bar', 'line', 'pie', etc.)
+            data: {
+                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+                datasets: [{
+                    label: 'Sample Data',
+                    data: [65, 59, 80, 81, 56, 55, 40],
+                    backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                    borderColor: 'rgba(75, 192, 192, 1)',
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+
+
+
+
 document.querySelector('.menu-btn').addEventListener('click', () => document.querySelector('.main-menu').classList.toggle('show'));
